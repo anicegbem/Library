fae71bff644fa088dce1f8121b8481ad063ec59f
anicegbem <matthewdlama4@gmail.com>
2023-02-18 15:23:43 +0100

    assign correct id to input(title) and (author)

--- library.js
+++ library.js
@@ -1,13 +1,18 @@
-let myLibrary = ['The Famished Road'];
+let myLibrary = [];
 let table = document.querySelector('table');
 let newbook = document.getElementById('new');
 let form = document.querySelector('form');
 let submit = document.getElementById('submit');
+let author = document.getElementById('author');
+let title = document.getElementById('title');
+let pages = document.getElementById('pages');
+let heads = document.querySelectorAll('th');
+let tbody = document.querySelector('tbody');
 
-function Book(title) {
+function Book(title, author, pages) {
     this.title = title
-    // this.author = author
-    // this.pages = pages
+    this.author = author
+    this.pages = pages
     // this.read = read
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages ${this.read}`;
@@ -15,20 +20,34 @@ function Book(title) {
 }
 
 function addBookToLibrary() {
-    // let input = prompt("Enter the books of choice");
-    // let book = new Book(input);
-    // myLibrary.push(book.title);
-    // displayBooks();
+    // create the object instance
+    let book = new Book(title.value, author.value, pages.value);
+    console.log(book);
+    // add the object to the array
+    myLibrary.push(book);
+    // display the items(title, pages, read, author) on screen
+    displayBooks();
     
 }
 
 function displayBooks() {
     for(let book of myLibrary) {
-        let data = document.createElement("td");
-        data.textContent = book;
+        let titledata = document.createElement('td');
+        titledata.textContent = book.title;
+        let authordata = document.createElement('td');
+        authordata.textContent = book.author;
+        let pagedata = document.createElement('td');
+        pagedata.textContent = book.pages;
         let row = document.createElement('tr');
-        row.append(data);
-        table.append(row);
+        row.append(titledata, authordata, pagedata);
+        tbody.append(row);
+        table.append(tbody);
+        
+        // let data = document.createElement("td");
+        // data.textContent = book.title;
+        // let row = document.createElement('tr');
+        // row.append(data);
+        // table.append(row);
 
     }
 }
@@ -46,6 +65,17 @@ function stopSubmit(event) {
     event.preventDefault();
 }
 
-addBookToLibrary();
+function submitBook() {
+    submit.addEventListener('click', function() {
+        if(title.value !== '' && author.value !== '' && pages.value !== '') {
+            addBookToLibrary();
+            
+        }
+        
+    })
+}
+
+
 addNewBooks();
+submitBook();
 
